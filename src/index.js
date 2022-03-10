b142077ea89d6dd33a06b3e2b5e8a512b4ae1e2d
Chanze Wu <chanze7995@gmail.com>
2022-03-10 10:44:30 +0800

    Create Header & Navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 console.log("Hi");
 // css
-// import '@/assets/scss/all.scss'
 import "./assets/scss/all.scss";
+// import "boxicons";
+// import "../node_modules/boxicons/css/boxicons.min.css";
 // js
