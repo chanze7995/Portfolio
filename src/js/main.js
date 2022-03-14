4222f840fd1ab8b1107e6455cfc83cade6e91f31
Chanze Wu <chanze7995@gmail.com>
2022-03-14 18:48:10 +0800

    Set Fn to Control Modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//CHANGE BACKGROUND HEADER
+//CHANGE BACKGROUND OF HEADER
 function scrollHeader() {
   const header = document.getElementById("header");
   if (this.scrollY >= 50) {
@@ -8,3 +8,26 @@ function scrollHeader() {
   }
 }
 window.addEventListener("scroll", scrollHeader);
+
+// CONTROL MODAL OF SERVICES SECTION
+const modalViews = document.querySelectorAll(".services__modal");
+const modalOpenBtn = document.querySelectorAll(".services__button");
+const modalCloseBtn = document.querySelectorAll(".services__modal");
+
+const openModal = (modalId) => {
+  modalViews[modalId].classList.add("active-modal");
+};
+const closeModal = (modalId) => {
+  modalViews[modalId].classList.remove("active-modal");
+};
+
+modalOpenBtn.forEach((item, id) => {
+  item.addEventListener("click", () => {
+    openModal(id);
+  });
+});
+modalCloseBtn.forEach((item, id) => {
+  item.addEventListener("click", () => {
+    closeModal(id);
+  });
+});
